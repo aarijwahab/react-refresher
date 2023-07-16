5be665ac3db35c15613a07b3b32f359f338c10fb
Aarij <aarijwahab@gmail.com>
2023-07-16 19:38:07 -0400

    added api data fetching and posting

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,5 +1,26 @@
+import NewMeetupForm from "../components/meetups/NewMeetupForm";
+import { useHistory } from "react-router-dom";
+
 const NewMeetupPage = () => {
-  return <div>New Meetup Page</div>;
+  const history = useHistory();
+
+  const handleAddMeetup = async (meetupData) => {
+    await fetch(`${process.env.REACT_APP_FIREBASE_URL}/meetups.json`, {
+      method: "POST",
+      body: JSON.stringify(meetupData),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    });
+    history.replace("/");
+  };
+
+  return (
+    <section>
+      <h1>Add New Meetup</h1>
+      <NewMeetupForm onAddMeetup={handleAddMeetup} />
+    </section>
+  );
 };
 
 export default NewMeetupPage;
